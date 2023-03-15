e12b265391a93d44dd58fedd68cf7c6899655c6b
MrADS2o3 <aridwis203@gmail.com>
2023-03-15 12:04:46 +0700

    First Commit

---
+++ Test Suites/Akun/LGN - Login/TS01_Login All Avail Account.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS01_Login All Avail Account</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>abc750fe-e4a3-4c71-9a9e-045e22ede0a6</testSuiteGuid>
+   <testCaseLink>
+      <guid>d3c4eda6-d683-46b1-9435-cebb9718fadc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Akun/LGN - Login/DD_Login Akun</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>487ad4ad-8c54-4bfa-84d2-b49b9ff1e767</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Akun/LGN - Login/P_Data Login</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>487ad4ad-8c54-4bfa-84d2-b49b9ff1e767</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Username</value>
+         <variableId>090719c6-9a4f-4fa7-b908-76f0058d8572</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>487ad4ad-8c54-4bfa-84d2-b49b9ff1e767</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Password</value>
+         <variableId>f18d8fa1-39bf-4c7d-82bf-8f10f2cf3884</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Akun/LGN - Login/TS02_Run All Login Test Case.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS02_Run All Login Test Case</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>36465f4f-6a51-4e76-a12d-69b472a0bd27</testSuiteGuid>
+   <testCaseLink>
+      <guid>74067b93-483b-4afc-94a7-f8dfdc9e2696</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Akun/LGN - Login/P_LGN01 Login Akun</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d4cc4e9f-8971-42c0-8ba7-3f166377d7b8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Akun/LGN - Login/N_LGN03 Login Akun</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a4c9c46f-9942-4cf4-8857-d07a39f69a87</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Akun/LGN - Login/N_LGN02 Login Akun</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
